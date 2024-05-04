fc968afeb419ca7b56e6b5f4e4634f52480fada3
jihogrammer <jihogrammer@gmail.com>
2024-05-04 18:15:03 +0900

    Add toolkit to link home

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@ import { initTheme } from "./utils/theme.js";
 import { registerHotKeys } from "./utils/hotkey.js";
 import * as QueryParameters from "./utils/params.js";
 import { renderEdit, renderView } from "./components/container.js";
-
+import { initToolkit } from "./utils/toolkit.js";
 
 window.onload = () => {
+  initToolkit();
   initTheme();
   registerHotKeys();
 
---
+++ js/utils/toolkit.js
@@ -0,0 +1,5 @@
+export const initToolkit = () => {
+  document.getElementById("home-icon").addEventListener("click", () => {
+    window.open("https://github.com/jihogrammer/json-comparator", "_blank");
+  });
+};
