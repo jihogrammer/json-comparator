1bf9c8271c4e78c29046f171fa3fd6d59b8c6cda
jihogrammer <jihogrammer@gmail.com>
2024-05-04 19:00:24 +0900

    Add donation toolkit

--- js/utils/toolkit.js
+++ js/utils/toolkit.js
@@ -1,5 +1,32 @@
+import { ModalContainer } from "../components/modal.js";
+
+const modal = new ModalContainer("donation");
+
+const DONATION_IMAGES = ["./img/donation-qr-toss.png", "./img/donation-qr-kakao.jpeg"].map((src) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.style.display = "block";
+  img.style.width = "30vw";
+  img.style.objectFit = "cover";
+  return img;
+});
+
 export const initToolkit = () => {
-  document.getElementById("home-icon").addEventListener("click", () => {
+  document.getElementById("home").addEventListener("click", () => {
     window.open("https://github.com/jihogrammer/json-comparator", "_blank");
+  });
+
+  document.getElementById("donation").addEventListener("click", () => {
+    const container = document.createElement("div");
+    container.style.display = "flex";
+    container.style.flexDirection = "row";
+    container.style.justifyContent = "center";
+    container.style.gap = "1rem";
+
+    for(const img of DONATION_IMAGES) {
+      container.appendChild(img);
+    }
+
+    modal.activate(container);
   });
 };
